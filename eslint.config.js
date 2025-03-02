7eb289f7d09c654c1aa22b66a9f4126650286ddf
Sai Kiran <neelakantamsaikiran2@gmail.com>
2025-03-02 13:29:57 +0530

    chore: repo setup

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist',"commitlint.config.ts", ".husky/"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
